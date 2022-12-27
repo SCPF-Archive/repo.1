ed30fc014cb8ad752cbec222faae9081a20edae8
Shadow Person <unbug.ph@gmail.com>
2022-12-26 23:03:25 -0600

    Update download.sh
    Signed-off-by: Shadow Person <unbug.ph@gmail.com>

--- revanced/packages/nyx/download.sh
+++ revanced/packages/nyx/download.sh
@@ -4,8 +4,6 @@ declare -A apks
 
 apks["com.awedea.nyx.apk"]="https://nyx-music-player.en.uptodown.com/android/download"
 
-WGET_HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
-
 get_apk_download_url()
 {
     version_url=$(curl -s "$1" | jq ".data[] | select(.version | contains(\"$2\")) | .versionURL")
