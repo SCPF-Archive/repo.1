f78c67ddf61d4d0f8496bc586ef15887b464cc29
Shadow Person <unbug.ph@gmail.com>
2022-12-27 01:26:51 -1100

    Update compile.sh
    Signed-off-by: Shadow Person <unbug.ph@gmail.com>

--- revanced/packages/compile.sh
+++ revanced/packages/compile.sh
@@ -5,8 +5,8 @@ patches_file=./tiktok.patch
 included_start="$(grep -n -m1 'INCLUDED PATCHES' "$patches_file" | cut -d':' -f1)"
 excluded_start="$(grep -n -m1 'EXCLUDED PATCHES' "$patches_file" | cut -d':' -f1)"
 
-included_patches="$(tail -n +$included_start $patches_file | head -n "$(( excluded_start - included_start ))" | grep '^[^#[:blank:]]')"
-excluded_patches="$(tail -n +$excluded_start $patches_file | grep '^[^#[:blank:]]')"
+included_patches="$(tail -n +"$included_start" "$patches_file" | head -n "$(( excluded_start - included_start ))" | grep '^[^#[:blank:]]')"
+excluded_patches="$(tail -n +"$excluded_start* "$patches_file" | grep '^[^#[:blank:]]')"
 
 ###################################
 
@@ -37,8 +37,8 @@ fi
 ###################################
 
 echo "CALL POPULATE PATCHES"
-[[ ! -z "$included_patches" ]] && populate_patches "-i" "$included_patches"
-[[ ! -z "$excluded_patches" ]] && populate_patches "-e" "$excluded_patches"
+[[ -n "$included_patches" ]] && populate_patches "-i" "$included_patches"
+[[ -n "$excluded_patches" ]] && populate_patches "-e" "$excluded_patches"
 
 ###################################
 
@@ -52,8 +52,8 @@ if [ -f "com.zhiliaoapp.musically.apk" ]
 then
     echo "PATCHING TIKTOK"
     java -jar cli.jar -m integrations.apk -b patches.jar \
-        ${patches[@]} \
-        $EXPERIMENTAL \
+        "${patches[@]}" \
+        "$EXPERIMENTAL" \
         -a com.zhiliaoapp.musically.apk -o output/tiktok.apk
 else
     echo "NO BASE PACKAGE, SKIP COMPILING TIKTOK"
