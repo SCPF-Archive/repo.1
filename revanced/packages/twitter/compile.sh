cd926408b71da67e75b4e20af70ede5ad3f2492a
Shadow Person <unbug.ph@gmail.com>
2022-12-28 21:27:54 +0900

    Create compile.sh
    Signed-off-by: Shadow Person <unbug.ph@gmail.com>

---
+++ revanced/packages/twitter/compile.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+patches_file=./twitter.patch
+
+included_start="$(grep -n -m1 'INCLUDED PATCHES' "$patches_file" | cut -d':' -f1)"
+excluded_start="$(grep -n -m1 'EXCLUDED PATCHES' "$patches_file" | cut -d':' -f1)"
+
+included_patches="$(tail -n +"$included_start" "$patches_file" | head -n "$(( excluded_start - included_start ))" | grep '^[^#[:blank:]]')"
+excluded_patches="$(tail -n +"$excluded_start" "$patches_file" | grep '^[^#[:blank:]]')"
+
+###################################
+
+populate_patches()
+{
+    while read -r revanced_patches
+    do
+        patches+=("$1 $revanced_patches")
+    done <<< "$2"
+}
+
+###################################
+
+echo "CLEANING UP"
+if [[ "$1" == "clean" ]]
+    then
+    rm -f revanced-cli.jar revanced-patches.jar
+    exit
+fi
+
+###################################
+
+echo "SET EXPERIMENTAL"
+if [[ "$1" == "experimental" ]]; then
+    EXPERIMENTAL="--experimental"
+fi
+
+###################################
+
+echo "FETCHING DEPENDENCIES"
+for artifact in "${!artifacts[@]}"
+do
+    if [ ! -f "$artifact" ]
+    then
+        echo "DOWNLOADING $artifact"
+        curl -sLo "$artifact" $(get_artifact_download_url ${artifacts[$artifact]})
+    fi
+done
+
+###################################
+
+echo "CALL POPULATE PATCHES"
+[[ -n "$included_patches" ]] && populate_patches "-i" "$included_patches"
+[[ -n "$excluded_patches" ]] && populate_patches "-e" "$excluded_patches"
+
+###################################
+
+echo "MAKING DIRECTORY"
+mkdir -p output
+
+###################################
+
+echo "COMPILING TWITTER"
+if [ -f "com.twitter.android.apk" ]
+then
+    echo "PATCHING TWITTER"
+    java -jar cli.jar -m integrations.apk -b patches.jar \
+        "${patches[@]}" \
+        "$EXPERIMENTAL" \
+        -a com.twitter.android.apk -o output/twitter.apk
+else
+    echo "NO BASE PACKAGE, SKIP COMPILING TWITTER"
+fi
+
+echo "DONE"
