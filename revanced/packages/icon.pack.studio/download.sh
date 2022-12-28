891547051fcb618c3f9bfe96d5fa205acc0c25a3
Shadow Person <unbug.ph@gmail.com>
2022-12-28 12:18:23 -0700

    Update download.sh
    Signed-off-by: Shadow Person <unbug.ph@gmail.com>

--- revanced/packages/icon.pack.studio/download.sh
+++ revanced/packages/icon.pack.studio/download.sh
@@ -5,8 +5,6 @@ declare -A apks
 
 apks["ginlemon.iconpackstudio.apk"]=dl_iconpackstudio
 
-WGET_HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
-
 req()
 { wget -nv -O "$2" --header="$WGET_HEADER" "$1"; }
 
