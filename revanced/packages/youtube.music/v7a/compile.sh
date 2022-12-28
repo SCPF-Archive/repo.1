b71cc3b91785956509f44cdc12e063fcb54ee6f1
Shadow Person <unbug.ph@gmail.com>
2022-12-28 15:31:25 +0300

    Create compile.sh
    Signed-off-by: Shadow Person <unbug.ph@gmail.com>

---
+++ revanced/packages/youtube.music/v7a/compile.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+patches_file=./youtube.music.patch
+
+included_start="$(grep -n -m1 'INCLUDED PATCHES' "$patches_file" | cut -d':' -f1)"
+excluded_start="$(grep -n -m1 'EXCLUDED PATCHES' "$patches_file" | cut -d':' -f1)"
+
+included_patches="$(tail -n +"$included_start" "$patches_file" | head -n "$(( excluded_start - included_start ))" | grep '^[^#[:blank:]]')"
+excluded_patches="$(tail -n +"$excluded_start" "$patches_file" | grep '^[^#[:blank:]]')"
+
+populate_patches()
+{
+    while read -r revanced_patches
+    do
+        patches+=("$1 $revanced_patches")
+    done <<< "$2"
+}
+
+echo "CLEANING UP"
+if [[ "$1" == "clean" ]]; then
+    rm -f revanced-cli.jar revanced-integrations.apk revanced-patches.jar
+    exit
+fi
+
+if [[ "$1" == "experimental" ]]; then
+    EXPERIMENTAL="--experimental"
+fi
+
+echo "CALL POPULATE PATCHES"
+[[ -n "$included_patches" ]] && populate_patches "-i" "$included_patches"
+[[ -n "$excluded_patches" ]] && populate_patches "-e" "$excluded_patches"
+
+echo "MAKING DIRECTORY"
+mkdir -p output
+
+echo "COMPILING YOUTUBE MUSIC arm-v7a"
+if [ -f "com.google.android.apps.youtube.music.apk" ]
+then
+    echo "PATCHING YOUTUBE MUSIC arm-v7a"
+    java -jar cli.jar -m integrations.apk -b patches.jar \
+        "${patches[@]}" \
+        "$EXPERIMENTAL" \
+        -a com.google.android.apps.youtube.music.apk -o output/yt.music.v7a.apk
+else
+    echo "NO BASE PACKAGE, SKIP COMPILING YOUTUBE MUSIC arm-v7a"
+fi
+
+echo "DONE"
