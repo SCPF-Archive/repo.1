c416d0fc3b60f7eff886cca469df99350c95d81a
Shadow Person <unbug.ph@gmail.com>
2023-01-01 10:16:43 +0800

    Update copy.latest.files.sh
    Signed-off-by: Shadow Person <unbug.ph@gmail.com>

--- revanced/copy.latest.files.sh
+++ revanced/copy.latest.files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "MAKING DIRECTORIES"
+mkdir -p packages/backdrops/versions
 mkdir -p packages/citra.emulator/versions
 mkdir -p packages/icon.pack.studio/versions
 mkdir -p packages/nyx/versions
@@ -13,6 +14,7 @@ mkdir -p packages/youtube.music/v7a/versions
 mkdir -p packages/youtube.music/v8a/versions
 
 echo "COPYING LATEST VERSION FILES"
+cp -p -v assets/versions/latest/versions.json packages/backdrops/versions
 cp -p -v assets/versions/latest/versions.json packages/citra.emulator/versions
 cp -p -v assets/versions/latest/versions.json packages/icon.pack.studio/versions
 cp -p -v assets/versions/latest/versions.json packages/reddit/versions
@@ -24,6 +26,7 @@ cp -p -v assets/versions/latest/versions.json packages/youtube.music/v7a/version
 mv -v assets/versions/latest/versions.json packages/youtube.music/v8a/versions
 
 echo "MOVING PATCH FILES"
+cp -p -v assets/patches/universal.patch packages/backdrops
 cp -p -v assets/patches/universal.patch packages/citra.emulator
 cp -p -v assets/patches/universal.patch packages/icon.pack.studio
 mv -v assets/patches/universal.patch packages/nyx
@@ -37,6 +40,7 @@ cp -p -v assets/patches/youtube.music.patch packages/youtube.music/v7a
 mv -v assets/patches/youtube.music.patch packages/youtube.music/v8a
 
 echo "COPYING REVANCED INTEGRATIONS"
+cp -p -v assets/temp/integrations/*.apk packages/backdrops/integrations.apk
 cp -p -v assets/temp/integrations/*.apk packages/citra.emulator/integrations.apk
 cp -p -v assets/temp/integrations/*.apk packages/icon.pack.studio/integrations.apk
 cp -p -v assets/temp/integrations/*.apk packages/nyx/integrations.apk
@@ -50,6 +54,7 @@ cp -p -v assets/temp/integrations/*.apk packages/youtube.music/v7a/integrations.
 mv -v assets/temp/integrations/*.apk packages/youtube.music/v8a/integrations.apk
 
 echo "COPYING REVANCED CLI"
+cp -p -v assets/temp/cli/*.jar packages/backdrops/cli.jar
 cp -p -v assets/temp/cli/*.jar packages/citra.emulator/cli.jar
 cp -p -v assets/temp/cli/*.jar packages/icon.pack.studio/cli.jar
 cp -p -v assets/temp/cli/*.jar packages/nyx/cli.jar
@@ -63,6 +68,7 @@ cp -p -v assets/temp/cli/*.jar packages/youtube.music/v7a/cli.jar
 mv -v assets/temp/cli/*.jar packages/youtube.music/v8a/cli.jar
 
 echo "COPYING REVANCED PATCHES"
+cp -p -v assets/temp/patches/*.jar packages/backdrops/patches.jar
 cp -p -v assets/temp/patches/*.jar packages/citra.emulator/patches.jar
 cp -p -v assets/temp/patches/*.jar packages/icon.pack.studio/patches.jar
 cp -p -v assets/temp/patches/*.jar packages/nyx/patches.jar
