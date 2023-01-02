64b29b1fca602aefc62a117edaf9a2e20b760d09
Shadow Person <unbug.ph@gmail.com>
2023-01-02 15:02:23 +0800

    Create download.sh
    Signed-off-by: Shadow Person <unbug.ph@gmail.com>

---
+++ revanced/download.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./packages/backdrops/download.sh
+./packages/citra.emulator/download.sh
+./packages/icon.pack.studio/download.sh
+./packages/nyx/download.sh
+./packages/reddit/download.sh
+./packages/spotify/download.sh
+./packages/tiktok/download.sh
+./packages/twitch/download.sh
+./packages/twitter/download.sh
+./packages/youtube.music/v7a/download.sh
+./packages/youtube.music/v8a/download.sh
+./packages/youtube/download.sh
